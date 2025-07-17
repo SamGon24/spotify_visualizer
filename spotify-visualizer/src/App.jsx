5b0bfe1dd271292970f197ec1d0b1f73ebf6ff05
SamGon24 <samuelitro24@gmail.com>
2025-07-17 09:16:18 -0500

    Adding tailwind

---
+++ spotify-visualizer/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-blue-600">
+        🎉 TailwindCSS funcionando en local
+      </h1>
+    </div>
+  );
+}
